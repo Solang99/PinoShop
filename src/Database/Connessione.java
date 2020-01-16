b2f5d342395becb4aaa0045c555e716418269b9a
Angelo <angelo.so@live.it>
2020-01-16 20:03:44 +0100

    Messo db online

--- src/Database/Connessione.java
+++ src/Database/Connessione.java
@@ -9,8 +9,8 @@ public class Connessione {
 	private static Connection con;
 	private static Properties props;
 	
-	private static String url = "jdbc:postgresql://localhost/pinoshop";
-	private static String user = "postgres";
+	private static String url = "jdbc:postgresql://manny.db.elephantsql.com:5432/fizsabrb";
+	private static String user = "amdextvy";
 	private static String driverName ="org.postgresql.Driver";
 	
 	
@@ -23,8 +23,8 @@ public class Connessione {
 			System.err.println("Classe non trovata");
 		}
     	props= new Properties();
-		props.setProperty("user","postgres");
-		props.setProperty("password","soli");
+		props.setProperty("user","fizsabrb");
+		props.setProperty("password","jVj81lg6sBIZpr_hPknGirdX3WYcLHJS");
     	
 		try {
 			con = DriverManager.getConnection(url, props);
