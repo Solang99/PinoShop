72491b3050b19bca658147469ce13299581a4db3
Angelo <angelo.so@live.it>
2020-01-16 23:29:08 +0100

    fix chiusura totale

--- src/GUI/AddArticoloFrame.java
+++ src/GUI/AddArticoloFrame.java
@@ -57,7 +57,7 @@ public class AddArticoloFrame extends JFrame {
 		URL url = getClass().getResource("/IconRegister/notfound.png");
 		File fotoFile = new File(url.getPath());
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 783, 643);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
